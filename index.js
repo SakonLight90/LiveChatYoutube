5a7e7f0e2b3bcbc7e3eb85aab22430cce4fbef7c
SakonLight90 <66551324+SakonLight90@users.noreply.github.com>
2024-06-22 18:52:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,118 +1,106 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// Importa il modulo 'path'
-const path = require('path');
-var express = require("express");
-var puppeteer_1 = require("puppeteer");
-var app = express();
-app.use(express.json());
-// Configura Express per servire i file statici dalla directory 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+const express = require("express");
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const app = express();
 
-function getImageSrc(url) {
-    return __awaiter(this, void 0, void 0, function () {
-        var browser, page, linkHref;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, puppeteer_1.default.launch({
-                        headless: true,
-                        args: ['--no-sandbox'] // Add the --no-sandbox flag here
-                    })];
-                case 1:
-                    browser = _a.sent();
-                    return [4 /*yield*/, browser.newPage()];
-                case 2:
-                    page = _a.sent();
-                    // Skip the banner asking to accept cookies
-                    return [4 /*yield*/, page.setCookie({
-                            name: "SOCS",
-                            value: "CAESEwgDEgk0ODE3Nzk3MjQaAmVuIAEaBgiA_LyaBg",
-                            domain: ".youtube.com",
-                        })];
-                case 3:
-                    // Skip the banner asking to accept cookies
-                    _a.sent();
-                    return [4 /*yield*/, page.goto(url, { waitUntil: "domcontentloaded" })];
-                case 4:
-                    _a.sent();
-                    return [4 /*yield*/, page.evaluate(function () {
-                            var linkTag = document.querySelector('link[rel="image_src"]');
-                            return linkTag ? linkTag.getAttribute("href") : null;
-                        })];
-                case 5:
-                    linkHref = _a.sent();
-                    return [4 /*yield*/, browser.close()];
-                case 6:
-                    _a.sent();
-                    return [2 /*return*/, linkHref];
-            }
-        });
+app.use(express.json());
+
+// Custom middleware to track IP addresses
+app.use((req, res, next) => {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const timestamp = new Date().toISOString();
+    const log = `${timestamp}: Request from IP: ${ip}\n`;
+    fs.appendFile("ip_logs.txt", log, (err) => {
+        if (err) console.error("Error writing IP log:", err);
     });
+    next();
+});
+
+// Function to check if a URL is reachable by Puppeteer
+async function isUrlReachable(url) {
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox'],
+        });
+        const page = await browser.newPage();
+        const response = await page.goto(url, { waitUntil: "domcontentloaded" });
+        await browser.close();
+        
+        if (response && response.status() === 200) {
+            return true; // URL is reachable
+        } else {
+            return false; // URL is not reachable or returned an error status
+        }
+    } catch (error) {
+        console.error("Error while checking URL reachability:", error);
+        return false; // Error occurred, URL is not reachable
+    }
 }
+
+// Function to get the image source URL from a YouTube live page
+async function getImageSrc(url) {
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+    });
+    const page = await browser.newPage();
+    // Set a cookie to skip the cookie consent banner
+    await page.setCookie({
+        name: "SOCS",
+        value: "CAESEwgDEgk0ODE3Nzk3MjQaAmVuIAEaBgiA_LyaBg",
+        domain: ".youtube.com",
+    });
+    await page.goto(url, { waitUntil: "domcontentloaded" });
+    const linkHref = await page.evaluate(() => {
+        const linkTag = document.querySelector('link[rel="image_src"]');
+        return linkTag ? linkTag.getAttribute("href") : null;
+    });
+    await browser.close();
+    return linkHref;
+}
+
+// Function to extract video ID from the image source URL
 function extractIdFromImageSrc(url) {
-    var match = url.match(/vi\/([^\/]*)\//);
+    const match = url.match(/vi\/([^\/]*)\//);
     return match ? match[1] : "";
 }
-app.get("/", function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
-    var imageSrc, errMsg, extractedId;
-    return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                if (!req.query.channel) {
-                    res.send("You are missing the 'channel' query parameter");
-                    return [2 /*return*/];
-                }
-                return [4 /*yield*/, getImageSrc("https://www.youtube.com/@".concat(req.query.channel, "/live"))];
-            case 1:
-                imageSrc = _a.sent();
-                errMsg = "This channel doesn't seem to be streaming as of right now or I was unable to fetch the live URL.";
-                if (!imageSrc || imageSrc === "")
-                    return [2 /*return*/, res.send(errMsg)];
-                extractedId = extractIdFromImageSrc(imageSrc);
-                if (!extractedId || extractedId === "")
-                    return [2 /*return*/, res.send(errMsg)];
-                res.redirect("https://www.youtube.com/live_chat?is_popout=1&v=" + extractedId);
-                return [2 /*return*/];
-        }
-    });
-}); });
-var port = process.env.PORT || 3001;
-app.listen(port, function () {
-    console.log("Listening on port ".concat(port));
+
+// Define the route to handle channel checking and redirection
+app.get("/", async (req, res) => {
+    if (!req.query.c) {
+        res.status(400).send("Missing 'c' query parameter");
+        return;
+    }
+
+    const channelUrl = `https://www.youtube.com/@${req.query.c}/live`;
+    const isReachable = await isUrlReachable(channelUrl);
+
+    if (!isReachable) {
+        res.status(404).send("The channel URL is not reachable.");
+        return;
+    }
+
+    const imageSrc = await getImageSrc(channelUrl);
+    const errMsg = "This channel doesn't seem to be streaming right now or I was unable to fetch the live URL.";
+
+    if (!imageSrc || imageSrc === "") {
+        res.status(404).send(errMsg);
+        return;
+    }
+
+    const extractedId = extractIdFromImageSrc(imageSrc);
+    if (!extractedId || extractedId === "") {
+        res.status(404).send(errMsg);
+        return;
+    }
+
+    res.redirect(`https://www.youtube.com/live_chat?is_popout=1&v=${extractedId}`);
+});
+
+// Start the server
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
 });
